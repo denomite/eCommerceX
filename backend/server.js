12d4b9ca161a13ae5a592dd8c688f07c495c8dac
Denis Soke <sokedenis@gmail.com>
2024-12-27 09:40:15 +0100

    serv&fetch

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import products from './data/products.js';
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    });
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+    }
+);
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(product => product._id === req.params.id);
+    res.json(product);
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+    });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <h1>Welcome to eCommerce Shop</h1>;
+  return (
+    <>
+     <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
